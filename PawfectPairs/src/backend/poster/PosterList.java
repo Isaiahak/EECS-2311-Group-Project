145be462f5485dedbb3ab0a0b730c29ccbcd9582
katya-koz <119365237+katya-koz@users.noreply.github.com>
2024-02-16 17:17:49 -0500

    Restructuring

---
+++ PawfectPairs/src/backend/poster/PosterList.java
@@ -0,0 +1,60 @@
+package backend.poster;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+
+import backend.dog.Dog;
+
+public class PosterList {
+	private TreeMap<String, Poster> PosterMap = new TreeMap<>();
+	
+    
+    public void addPoster(String name, Poster poster) {//name of poster and poster object
+    	PosterMap.put(name, poster);
+    }
+    
+    public TreeMap<String, Poster> getDogMap() {
+        return PosterMap;
+    }
+    
+    public Poster getPoster(String name) {
+        return PosterMap.get(name);
+    }
+    
+    public TreeMap<String, Dog> getPosterDogs(Poster poster, TreeMap<String, Dog> dogMap) {
+    	TreeMap<String, Dog> PosterDogsMap = new TreeMap<>();
+    
+    	for (Dog dog : dogMap.values()) {
+            if (dog.getPoster().equals(poster)) { //implement Comparable Interface: ->finished comparable interface for poster
+                PosterDogsMap.put(dog.getName(), dog);
+            }
+    	}
+        return PosterDogsMap;
+    }
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+        sb.append("PosterList [");
+        
+        // Get the set of keys from the TreeMap
+        Set<String> keys = PosterMap.keySet();
+        
+        // Iterate over the keys and append them to the StringBuilder
+        for (String key : keys) {
+            sb.append(key).append(", ");
+        }
+        
+        // Remove the trailing ", " and close the string
+        if (!PosterMap.isEmpty()) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        sb.append("]");
+        
+        return sb.toString();
+	}
+    
+    
+    
+}
---
+++ PawfectPairs/src/backend/user/User.java
@@ -0,0 +1,92 @@
+package backend.user;
+
+import java.util.ArrayList;
+
+import backend.dog.Dog;
+import backend.tag.Tag;
+
+public class User {
+	
+	private ArrayList<Tag> preferences =  new ArrayList<Tag>();
+	
+	private String username;
+	
+	private String email;
+	
+	private String password;
+	
+	private Wallet wallet;
+	
+	private ArrayList<Dog> likedDogs = new ArrayList<Dog>();
+
+	
+	
+	public User(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public ArrayList<Tag> getPreferences() {
+		return preferences;
+	}
+
+	public void setPreferences(ArrayList<Tag> preferences) {
+		this.preferences = preferences;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Wallet getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+
+	public ArrayList<Dog> getLikedDogs() {
+		return likedDogs;
+	}
+
+	public void setLikedDogs(ArrayList<Dog> likedDogs) {
+		this.likedDogs = likedDogs;
+	}
+	
+	 public ArrayList<Tag> getTags() {
+	        return preferences;
+	    }
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + ", password="
+				+ password + "]";
+	}
+	
+	
+	
+
+}
