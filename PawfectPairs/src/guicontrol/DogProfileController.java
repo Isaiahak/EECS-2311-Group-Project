45cae466eb28a2628e1cee4e6d572a2db9a1e7d5
Youchair <sidneyliu272102@gmail.com>
2024-02-25 01:09:32 -0500

    db connection stable. methods created to access dogs&posters from db

--- PawfectPairs/src/guicontrol/DogProfileController.java
+++ PawfectPairs/src/guicontrol/DogProfileController.java
@@ -1,7 +1,9 @@
 package guicontrol;
 
 import java.util.ArrayList;
+
 import java.util.List;
+import java.util.TreeMap;
 
 import backend.dog.Dog;
 import backend.poster.Poster;
@@ -12,19 +14,99 @@ import javafx.scene.image.*;
 import javafx.scene.text.*;
 import javafx.stage.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
 public class DogProfileController {
     private int currentIndex = 0;
-    private ArrayList<Dog> dogProfiles = new ArrayList<>(List.of(
+    private TreeMap <Integer, Poster> posterProfiles; 
+    private TreeMap <Integer, Dog> dogProfiles; 
     
+//    private DatabaseConnection connection = new DatabaseConnection();
+    
+    
+    //public DogProfileController() throws SQLException {
+    
+   private TreeMap <Integer, Poster> makePosters (){
+	   posterProfiles = new TreeMap<>();
+	   try(Connection connection = DatabaseConnection.connect()){
+		   Statement statement = connection.createStatement () ;
+		   ResultSet resultSet = statement.executeQuery ("SELECT * FROM poster") ;
+		   while (resultSet.next ()) {
+			   Poster poster = new Poster (resultSet.getInt("poster_id"), resultSet.getString("displayname"),resultSet.getInt("score")); 
+			   posterProfiles.put(resultSet.getInt("poster_id"), poster);
+		   }
+		   connection.close();
+	   
+		   
+		   
+	   }catch (SQLException e) {
+			System.out.println ("Connection failure.") ;
+			e.printStackTrace ();
+	   
+   }
+	   return posterProfiles;
+   }
+   
+    
+    private TreeMap <Integer, Dog> makeDogs (){
+    	 dogProfiles = new TreeMap<>();
+    			
+    		    try(Connection connection = DatabaseConnection.connect()){
+    				Statement statement = connection.createStatement () ;
+    				ResultSet resultSet = statement.executeQuery ("SELECT * FROM dog") ;
+    				
+    				//ResultSet resultSetPoster = statement.executeQuery("SELECT * FROM poster");
+    				
+    				
+    				
+    				 while (resultSet.next ()) {
+    					 
+    	
+    					 
+    					 	Dog dog = new Dog(resultSet.getString ("dogname"), resultSet.getInt("dogid"), resultSet.getInt("ageid"),  resultSet.getInt("energyid"), resultSet.getInt("sizeid"), resultSet.getInt("sexid"), posterProfiles.get(resultSet.getInt("posterid")), resultSet.getBoolean("adopted"), 
+    					 			resultSet.getString("imagePath"), resultSet.getString("biography"));
+    					 	 dogProfiles.put(resultSet.getInt("dogid"), dog);
+    					 	 //   int sex, Poster poster, boolean adopted, String imagePath, String biography)
+    					 	//dogid | dogname | adopted | biography | imagepath | posterid | ageid | energyid | sizeid | sexid
+    					 	
+    				 }
+    				 connection.close () ;
+    		           
+    		       } catch (SQLException e) {
+    		 			System.out.println ("Connection failure.") ;
+    		 			e.printStackTrace () ;
+    		}
+    		  
+    		    
+    		    
+		return dogProfiles;
+    	
+    }
+    
+    public void initialization () {
+    	makePosters();
+    	makeDogs();
+    }
+    
+      
+    
+    
+   /* private ArrayList<Dog> dogProfiles = new ArrayList<>(List.of(
+    
+    		
+
+    		new Dog("Charles", 1, 2, 1, 2, 1, new Poster(5, "doglover123", 12), false, "file:src/guilayout/dog.png", "Charles is involed in some shady stuff. Don't ask questions you don't want to know the answers to."), 
+    		new Dog("Sandy", 2, 3, 1, 2, 0, new Poster(5, "doglover123", 12), false,"file:src/guilayout/dog1.png", "Sandy looks like she knows something you don't... but that's what makes her so loveable!"),
+    		new Dog("Chuckles", 3, 3, 0, 0, 1, new Poster(5, "doglover123", 12), false,"file:src/guilayout/dog4.png", "Lazy and old Chuckles is the perfect lap dog for anyone who doesn't like to move.")
 
 
-//    		new Dog("Charles", 1, 2, 1, 2, 1, new Poster(5, "doglover123", 12), false, "file:src/guilayout/dog.png", "Charles is involed in some shady stuff. Don't ask questions you don't want to know the answers to."), 
-//    		new Dog("Sandy", 2, 3, 1, 2, 0, new Poster(5, "doglover123", 12), false,"file:src/guilayout/dog1.png", "Sandy looks like she knows something you don't... but that's what makes her so loveable!"),
-//    		new Dog("Chuckles", 3, 3, 0, 0, 1, new Poster(5, "doglover123", 12), false,"file:src/guilayout/dog4.png", "Lazy and old Chuckles is the perfect lap dog for anyone who doesn't like to move.")
 
-
-
-));
+));*/
     
     private Label primaryInfoLabel;
     private Label sizeLabel;
