62a5729374da7005669afd8d0f2a6017a0888452
Edson <edkenta@gmail.com>
2024-03-11 21:23:15 -0400

    JUnitCalendar

---
+++ PawfectPairs/test/backend/calendar/AppointmentManager.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppointmentManager {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
