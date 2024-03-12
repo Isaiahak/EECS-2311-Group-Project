3202bf84e900a21649873994bded631231a7d537
a-zainab <ansari.zainab.a8@gmail.com>
2024-03-11 20:39:55 -0400

    removing unnecesary code in wallet test

--- PawfectPairs/test/backend/wallet/WalletTest.java
+++ PawfectPairs/test/backend/wallet/WalletTest.java
@@ -47,52 +47,11 @@ class WalletTest {
 	}
 
 	@Test
-	public void testDonateInsufficientFunds() throws Wallet.FundsTooLow {
-		wallet.donate(150.0, poster); // Trying to donate more than the available balance
+	public void testPosterFunds(){
+		wallet.donate(10.0, poster); 
+		double actual= poster.getBalance();
+		assertEquals(10.0, actual);
 	}
 
 
 }
-/*
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import org.junit.Before;
-
-import backend.poster.Poster;
-import backend.user.User;
-import backend.wallet.Wallet;
-
-public class WalletTest {
-
-	public Wallet wallet;
-	public Poster poster;
-	public User user;
-	@Before
-	public void setUp() {
-		// Initialize a wallet with some initial values for testing
-		// wallet = new Wallet(100.0, true, 24, 123, 50, 0, 0);
-		user= new User("a", "a");
-		wallet = new Wallet(50.0,user.getUserID());
-		poster =new Poster(8, "testingPoster", 1, "123456789", "testingPoster@hotmail.com", 0.0);
-	}
-
-	@Test
-	public void testDeposit() {
-		wallet.deposit(50.0);
-		assertEquals(100.0, wallet.getBalance());
-	}
-
-	@Test
-	public void testDonateSufficientFunds() throws Wallet.FundsTooLow {
-		wallet.donate(30.0, poster);
-		assertEquals(70.0, wallet.getBalance(), 0.01);
-	}
-
-	@Test(expected = Wallet.FundsTooLow.class)
-	public void testDonateInsufficientFunds() throws Wallet.FundsTooLow {
-		wallet.donate(150.0, poster); // Trying to donate more than the available balance
-	}
-
-	
-}
-*/
