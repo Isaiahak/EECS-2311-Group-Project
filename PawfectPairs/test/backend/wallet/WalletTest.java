3ee21a2fb4d21f49bff4690d116288709dc7b6ed
a-zainab <ansari.zainab.a8@gmail.com>
2024-03-11 21:06:08 -0400

    more test cases

--- PawfectPairs/test/backend/wallet/WalletTest.java
+++ PawfectPairs/test/backend/wallet/WalletTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import backend.dog.Dog;
 import backend.poster.Poster;
 import backend.user.User;
+import backend.wallet.RecurringPayment;
 import backend.wallet.Wallet;
 class WalletTest {
 	public Wallet wallet;
 	public Poster poster;
 	public User user;
+	public Dog dog;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		
@@ -26,8 +30,9 @@ class WalletTest {
 		user= new User("a", "a");
 		wallet = new Wallet(50.0,user.getUserID());
 		poster =new Poster(8, "testingPoster", 1, "123456789", "testingPoster@hotmail.com", 0.0);
+		dog = new Dog("Mr.dog", 1, 3, 0, 1, 0, poster.getUniqueId(), false, "", "Mr.dog is a good dog");
+       //  dog = new Dog("Max", 1, 3, 0, 1, 0, poster.getUniqueId(), false,null,"bio");
 
-	
 	}
 
 	@AfterEach
@@ -53,5 +58,16 @@ class WalletTest {
 		assertEquals(10.0, actual);
 	}
 
+	
+	
 
+
+	@Test
+	public void recurringPayments(){
+		int amountToDonate =1;
+		int daysBetweenPayments=7;
+		
+		wallet.addRecurringPayment(new RecurringPayment(amountToDonate, daysBetweenPayments, dog.getId(), dog.getPosterId()));
+	}
+	
 }
