b768e45a3bc605019f6e2e48e3588cea4b4d8dd7
isaiah <isaiahkenny321123@gmail.com>
2024-03-23 14:06:28 -0400

    fixed the problem

--- PawfectPairs/src/guilayout/LikedDogScene.java
+++ PawfectPairs/src/guilayout/LikedDogScene.java
@@ -107,6 +107,7 @@ public class LikedDogScene extends PrimaryScene{
 	}
 	
 	public void initializeSmallerDogList( ArrayList<Dog> allDogs, ObservableList<Dog> currentList){
+		int addAmount = allDogs.size() > 20 ? 20 : allDogs.size();
 		for(int i = 0; i < 20; i++) {
 			dogsDisplayed = currentList.size();
 			currentList.add(allDogs.get(i));
