ad2fec7db61efd088480ba5ebb418f21f545d03e
ConnorMark <157181976+ConnorMark@users.noreply.github.com>
2024-03-23 23:18:27 -0400

    Update TagTest.java

--- PawfectPairs/test/backend/UnitTestingfolder/tag/TagTest.java
+++ PawfectPairs/test/backend/UnitTestingfolder/tag/TagTest.java
@@ -1,40 +1,70 @@
 package test.backend.tag;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import backend.tag.Tag;
 
-class TagTest {
-
+public class TagTest {
+    private Tag tag;
+    
+    
+    
     @Test
-    void test1() {
-        Tag tag1 = new Tag("Shy", 0);
-        int expected = 0;
-        assertEquals(expected, tag1.getWeight());
+    public void testGetTagName() {
+    	tag = new Tag("Friendly", 1);
+        assertEquals("Friendly", tag.getTagName());
     }
-
+    
     @Test
-    void test2() {
-        Tag tag1 = new Tag("Shy",0);
-        String expected = "Shy";
-        assertEquals(expected, tag1.getTagName());
+    public void testSetTagName() {
+    	tag = new Tag("Friendly", 1);
+        tag.setTagName("Active");
+        assertEquals("Active", tag.getTagName());
     }
-
+    
     @Test
-    void test3() {
-        Tag tag1 = new Tag("Angry",0);
-        tag1.setTagName("Shy");
-        String expected = "Shy";
-        assertEquals(expected, tag1.getTagName());
+    public void testGetTagId() {
+    	tag = new Tag("Friendly", 1);
+        assertEquals(1, tag.getTagId());
     }
-
+    
     @Test
-    void test4() {
-        Tag tag1 = new Tag("Shy",0);
-        tag1.setWeight(3);
-        int expected = 3;
-        assertEquals(expected, tag1.getWeight());
+    public void testSetTagId() {
+    	tag = new Tag("Friendly", 1);
+        tag.setTagId(2);
+        assertEquals(2, tag.getTagId());
+    }
+    
+    @Test
+    public void testGetWeight() {
+    	tag = new Tag("Friendly", 1);
+        assertEquals(0, tag.getWeight());
+    }
+    
+    @Test
+    public void testSetWeight() {
+    	tag = new Tag("Friendly", 1);
+        tag.setWeight(3);
+        assertEquals(3, tag.getWeight());
+    }
+    
+    @Test
+    public void testEquals() {
+    	tag = new Tag("Friendly", 1);
+        Tag sameTag = new Tag("Friendly", 1);
+        Tag differentTag = new Tag("Energetic", 2);
+        
+        assertTrue(tag.equals(sameTag)); // Same tag name and ID
+        assertFalse(tag.equals(differentTag)); // Different tag name and ID
     }
 }
-
