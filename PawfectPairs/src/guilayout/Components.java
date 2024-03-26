857c94a55c27ec8d68f7ba585effa6222fcad62e
Edson <edkenta@gmail.com>
2024-03-26 00:45:39 -0400

    Fixing adopted logic based on main

--- PawfectPairs/src/guilayout/Components.java
+++ PawfectPairs/src/guilayout/Components.java
@@ -668,7 +668,6 @@ public class Components{
 				{
 					
 					showAlert("Dog has been adopted", dog.getName() + " is thankful for you!", AlertType.INFORMATION);
-					dog.setAdopted(true);
 					AppointmentManager userManager = AppData.getInstance().getAppointmentManager();
 					ArrayList<Appointment> userAppointments = userManager.getUserAppointments();
 					for (Appointment appointment : userAppointments) {
@@ -678,6 +677,13 @@ public class Components{
 						}
 					}
 					AppData.getInstance().getUser().getWallet().removeRecurringPayment(dog.getId());
+					
+					ArrayList<Dog> likedDogList = AppData.getInstance().getUser().getLikedDogs();
+					for (Dog d:likedDogList) {
+						if (d.getId()==dog.getId()) {
+							d.setAdopted(true);
+						}
+					}
 					LikedDogScene likedPage = LikedDogScene.getInstance();
 					likedPage.start(primaryStage);
 					
